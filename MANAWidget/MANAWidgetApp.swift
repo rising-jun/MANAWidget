c96a79066e764a86c176c305985b86fd60f87d64
kdj <bonybony@photowidget.net>
2023-09-02 15:43:39 +0900

    project init

---
+++ MANAWidget/MANAWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MANAWidgetApp.swift
+//  MANAWidget
+//
+//  Created by 김동준 on 2023/09/02.
+//
+
+import SwiftUI
+
+@main
+struct MANAWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
